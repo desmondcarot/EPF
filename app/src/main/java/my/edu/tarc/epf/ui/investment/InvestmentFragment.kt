7a9019451c38dc9bb3378f58a739f95b31670061
anreh <anurehka@gmail.com>
2023-03-10 18:02:07 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/epf/ui/investment/InvestmentFragment.kt
@@ -0,0 +1,103 @@
+package my.edu.tarc.epf.ui.investment
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import my.edu.tarc.epf.R
+import my.edu.tarc.epf.databinding.FragmentInvestmentBinding
+import java.util.*
+import java.util.Calendar.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class InvestmentFragment : Fragment() {
+
+    private var _binding: FragmentInvestmentBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentInvestmentBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonDOB.setOnClickListener {
+            val dateDialogFragment = DateDialogFragment{
+                year, month, day -> onDateSelected(year, month, day)
+            }
+            dateDialogFragment.show(parentFragmentManager, "DatePicker")
+        }
+        binding.buttonCalculate.setOnClickListener {}
+        binding.buttonReset.setOnClickListener{}
+    }
+
+    private fun onDateSelected(year: Int, month: Int, day: Int) {
+        binding.buttonDOB.text = String.format("%d/%d/%d", day,month+1,year)
+
+        val dob = getInstance()
+        with(dob){
+            set(YEAR, year)
+            set(MONTH, month)
+            set(DAY_OF_MONTH, day)
+        }
+
+        val today = getInstance()
+        val age = daysBetween(dob, today).div(365)
+
+        val textage = binding.textViewAge
+        textage.text = String.format("%d",age)
+
+    }
+
+    private fun daysBetween(dob: Calendar, today: Calendar): Long {
+        var startDate = dob.clone() as Calendar
+        var daysBetween: Long = 0
+        while(startDate.before(today)){
+            startDate.add(DAY_OF_MONTH,1)
+            daysBetween++
+        }
+        return daysBetween
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    class DateDialogFragment(val dateSetListener:(year: Int, month: Int, day: Int) -> Unit): DialogFragment(), DatePickerDialog.OnDateSetListener {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val c = Calendar.getInstance()
+            val year = c.get(YEAR)
+            val month = c.get(MONTH)
+            val day = c.get(DAY_OF_MONTH)
+
+            return DatePickerDialog(requireContext(), this, year, month, day)
+        }
+
+        override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+            dateSetListener(year, month, day)
+        }
+
+    }
+}
